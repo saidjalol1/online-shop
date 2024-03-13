bdc7823bb2602b953d12df6ac7efa839c9f8f820
saidjalol1 <turakhujayevsaidjalol06@gmail.com>
2024-03-13 15:06:07 +0500

    .

--- shop/signals.py
+++ shop/signals.py
@@ -3,18 +3,45 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Order
 from .models import Notification
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+
+
 
 
 
 
 def send_notification_to_admin(order_instance):
-    Notification.objects.create(
+    notification = Notification.objects.create(
         order = order_instance,
         message=f'Yangi Buyurtma ID-{order_instance.id}.'
     )
+    channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body= notification.message)
 
 
 @receiver(post_save, sender=Order)
 def notify_admin_on_new_order(sender, instance, created, **kwargs):
     if created:
         send_notification_to_admin(instance)
+
+
+
+
+
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body}")
+
+
+channel.basic_consume(queue='hello',
+                      auto_ack=True,
+                      on_message_callback=callback)
